f274738fb7077cafe4ecaf6726814843e8ec892c
laksh <kadarisailaxman@gmail.com>
2025-05-31 22:12:18 +0530

    Updated import for Redeploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,14 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
-
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
-  return (
-    <Container fluid className="footer">
-      <Row>
+   return (
+     <Container fluid className="footer">
+       <Row>
         
        
         <Col md="4" className="footer-body">
